6f21229874ec89ae572dbe49d324843ee9127af2
AKRNANDULA <adirosgaz@gmail.com>
2026-02-03 11:40:14 +0530

    Simulation Updated

--- experiment/simulation/js/adiknob.js
+++ experiment/simulation/js/adiknob.js
@@ -44,7 +44,7 @@ function adi(){
     o1 = {width: Math.round(Number($("#adisimg").width())*0.08),  min:25, max:41, minorTicks: 5};
     o2 = {width: Math.round(Number($("#adisimg").width())*0.08),  min:20, max:30, minorTicks: 5};
     o3 = {width: Math.round(Number($("#adisimg").width())*0.08),  min:180, max:360, minorTicks: 5};  
-    d1 = google.visualization.arrayToDataTable([['Label', 'Value'],['Arc Speed', Number($("#ar1").val())]]);
+    d1 = google.visualization.arrayToDataTable([['Label', 'Value'],['Welding Speed', Number($("#ar1").val())]]);
     d2 = google.visualization.arrayToDataTable([['Label', 'Value'],['Volt. (V)', Number($("#ar2").val())]]);
     d3 = google.visualization.arrayToDataTable([['Label', 'Value'],['Amp. (A)', Number($("#ar3").val())]]);
     c1 = new google.visualization.Gauge(document.getElementById("adi1"));
@@ -53,12 +53,12 @@ function adi(){
     c1.draw(d1, o1);
     c2.draw(d2, o2);
     c3.draw(d3, o3);
-    $(".adik1").css({'z-index':'1', 'left':0.30*Number($("#adisimg").width()), 'top':-0.59*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
-    $(".adig1").css({'z-index':'1', 'left':0.43*Number($("#adisimg").width()),'top':-0.6*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
-    $(".adik2").css({'z-index':'2', 'left':0.695*Number($("#adisimg").width()), 'top':-0.5*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
-    $(".adig2").css({'z-index':'2', 'left':0.82*Number($("#adisimg").width()),'top':-0.5*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
-    $(".adik3").css({'z-index':'2', 'left':0.695*Number($("#adisimg").width()), 'top':-0.4*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
-    $(".adig3").css({'z-index':'2', 'left':0.82*Number($("#adisimg").width()),'top':-0.4*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
+    $(".adik1").css({'z-index':'1', 'left':0.35*Number($("#adisimg").width()), 'top':-0.60*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
+    $(".adig1").css({'z-index':'1', 'left':0.46*Number($("#adisimg").width()),'top':-0.61*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
+    $(".adik2").css({'z-index':'2', 'left':0.71*Number($("#adisimg").width()), 'top':-0.51*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
+    $(".adig2").css({'z-index':'2', 'left':0.82*Number($("#adisimg").width()),'top':-0.52*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
+    $(".adik3").css({'z-index':'2', 'left':0.71*Number($("#adisimg").width()), 'top':-0.42*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
+    $(".adig3").css({'z-index':'2', 'left':0.82*Number($("#adisimg").width()),'top':-0.42*Number($("#adisimg").width()), 'margin-bottom':'-6.5em'});
     
   }
 
